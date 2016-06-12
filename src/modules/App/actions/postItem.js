fffe915ac87c8d7a854a87bc075348d363e75a78
Christian Alfoni <christianalfoni@gmail.com>
2016-06-12 21:40:26 +0200

    added post item

--- src/modules/App/actions/postItem.js
+++ src/modules/App/actions/postItem.js
@@ -1,6 +1,13 @@
-function postItem({input, state, output}) {
-    throw new Error('Unimplemented cerebral action postItem');
+function postItem({state, output, services}) {
+  const items = state.get('app.items')
+
+  services.http.post('/items', items[items.length - 1])
+    .then(output.success)
+    .catch(output.error)
+
 }
+
+postItem.async = true
 postItem.outputs = ['success', 'error'];
 
 export default postItem;
