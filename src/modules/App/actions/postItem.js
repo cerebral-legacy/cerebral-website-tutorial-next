9a79da030fe7e630aa79334388f4ffd62d6ed7d8
Christian Alfoni <christianalfoni@gmail.com>
2016-06-12 21:43:07 +0200

    Updated posting item

--- src/modules/App/actions/postItem.js
+++ src/modules/App/actions/postItem.js
@@ -1,7 +1,8 @@
 function postItem({state, output, services}) {
   const items = state.get('app.items')
+  const lastItem = items[items.lenght - 1]
 
-  services.http.post('/items', items[items.length - 1])
+  services.http.post('/items', lastItem)
     .then(output.success)
     .catch(output.error)
 
