7eadc4bbcf9080cd2279c1a249a75c54550aaf5b
Christian Alfoni <christianalfoni@gmail.com>
2016-07-06 12:51:17 +0200

    changed signature

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -2,52 +2,48 @@ import React from 'react'
 import {connect} from 'cerebral-view-react'
 import Items from '../Items'
 
-// When we want to use lifecycle methods the
-// connect can be used as a decorator as well
-// for stateful components
-@connect({
+export default connect({
   newItemTitle: 'app.newItemTitle',
   isSaving: 'app.isSaving',
   error: 'app.error'
-})
-class App extends React.Component {
-  componentDidUpdate(prevProps) {
-    if (prevProps.isSaving && !this.props.isSaving) {
-      this.input.focus()
+},
+  class App extends React.Component {
+    componentDidUpdate(prevProps) {
+      if (prevProps.isSaving && !this.props.isSaving) {
+        this.input.focus()
+      }
+    }
+    onFormSubmit(event) {
+      event.preventDefault()
+      this.props.signals.app.newItemTitleSubmitted()
+    }
+    onInputChange(event) {
+      this.props.signals.app.newItemTitleChanged({
+        title: event.target.value
+      })
+    }
+    render() {
+      return (
+        <div>
+          <form onSubmit={event => this.onFormSubmit(event)}>
+            <input
+              autoFocus
+              type="text"
+              ref={node => this.input = node}
+              disabled={this.props.isSaving}
+              value={this.props.newItemTitle}
+              onChange={event => this.onInputChange(event)}
+            />
+            {
+              this.props.error ?
+                <span style={{color: 'red', paddingLeft: '10px'}}>{this.props.error}</span>
+              :
+                null
+            }
+          </form>
+          <Items />
+        </div>
+      )
     }
   }
-  onFormSubmit(event) {
-    event.preventDefault()
-    this.props.signals.app.newItemTitleSubmitted()
-  }
-  onInputChange(event) {
-    this.props.signals.app.newItemTitleChanged({
-      title: event.target.value
-    })
-  }
-  render() {
-    return (
-      <div>
-        <form onSubmit={event => this.onFormSubmit(event)}>
-          <input
-            autoFocus
-            type="text"
-            ref={node => this.input = node}
-            disabled={this.props.isSaving}
-            value={this.props.newItemTitle}
-            onChange={event => this.onInputChange(event)}
-          />
-          {
-            this.props.error ?
-              <span style={{color: 'red', paddingLeft: '10px'}}>{this.props.error}</span>
-            :
-              null
-          }
-        </form>
-        <Items />
-      </div>
-    )
-  }
-}
-
-export default App
+)
--- src/components/Items/index.js
+++ src/components/Items/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import {connect} from 'cerebral-view-react'
 
-export default connect(
-  {
-    items: 'app.items'
-  },
+export default connect({
+  items: 'app.items'
+},
   function Items(props) {
     return (
       <ul>
